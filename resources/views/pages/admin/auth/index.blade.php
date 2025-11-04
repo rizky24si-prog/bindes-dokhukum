ffeb23da3bfcfc075ae1879fe75707f10847d3d4
rizky24si-prog <rizky24@mahasiswa.pcr.ac.id>
2025-11-04 08:59:42 +0700

    perbaikan template : halaman login

--- resources/views/pages/admin/auth/index.blade.php
+++ resources/views/pages/admin/auth/index.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
 <main>
-    <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center" style="background-image: url('{{ asset('assets-admin/img/bg-desa.png') }}'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+    <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center" style="background-image: url('{{ asset('assets-admin/img/bg-desa.jpeg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat;">
         <div class="container">
             <div class="row justify-content-center form-bg-image" data-background-lg="{{ asset('assets-admin/img/illustrations/signin.svg') }}">
                 <div class="col-12 d-flex align-items-center justify-content-center">
                     <div class="bg-white shadow border-0 rounded border-light p-4 p-lg-5 w-100 fmxw-500" style="background-color: rgba(255, 255, 255, 0.95);">
                         <div class="text-center text-md-center mb-4 mt-md-0">
                             <h1 class="mb-0 h3">Login Ke Bina Desa</h1>
+                            <p  class="mb-0 mt-1"> Pusat Pelayanan Dokumen Desa</p>
                         </div>
 
                         @if(session('success'))
